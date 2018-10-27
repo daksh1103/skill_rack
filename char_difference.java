c00852c3169ec5377fa55451a5b8cc523b2daf46
daksh1103 <44317756+daksh1103@users.noreply.github.com>
2018-10-27 21:51:51 +0530

    Create char_difference.java
    Print the difference between characters

---
+++ char_difference.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char a=sc.next().charAt(0);
+		char b=sc.next().charAt(0);
+		int c=(int)a;
+		int d=(int)b;
+		int e=c-d;
+		int h=e+64;
+		char ch=(char) h;
+		System.out.print(ch);
+	    
+	}
+}
