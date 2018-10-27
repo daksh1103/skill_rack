e4c4946005773a6b8af24a0f622850dfb3f42c57
daksh1103 <44317756+daksh1103@users.noreply.github.com>
2018-10-27 23:18:21 +0530

    Create turning_pages.java
    Turning pages program in skillrack

---
+++ turning_pages.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=sc.nextInt();
+		int count1=0;
+		int count=0;
+		int m=0;
+for(int i=1;i<p;i++){//I==1 IS WHERE U WILL TURN THE PAGE.
+    count++;
+}
+if(n%2==1){
+    m=n-1;
+}
+else{
+    m=n;}
+   for(int i=m;i>p;i--){I==M WHERE THE PAGE IS TO BE TURNED.
+       count1++;
+   } 
+}
+	
+		if(count>count1){
+		    System.out.print(count1);
+		}
+		else{
+		    System.out.print(count);
+		}
+		
+		
+		
+		
+
+	}
+}
+}
